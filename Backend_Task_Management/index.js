f6fabdf396ec6c5fabe02282a41b6708d8a43f82
Arnav Jain <arnavjain92105@gmail.com>
2025-07-31 17:05:19 +0530

    Backend changes(1)

--- Backend_Task_Management/index.js
+++ Backend_Task_Management/index.js
@@ -14,7 +14,10 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors(
+  "http://localhost:5173",
+  "https://algonive-task-management-for-small.vercel.app"
+));
 app.use(express.json());
 
 // API Routes
